2ac5251c003403882311e9bdedf115518c243359
sungyeon1 <sungyeon0320@gmail.com>
2022-02-14 11:00:56 +0900

    without web3

---
+++ webpage/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <img src="./imgs/nft.png" className="nftpic" alt="nft" />
+      <h2>Discover, collect, and sell extraordinary NFTs</h2>
+    </div>
+  );
+}
+
+export default Homepage;
