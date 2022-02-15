17ea1d74259dc69f484090a17ea1f6c6410cc778
koeunchorong <koasis03@gmail.com>
2022-02-15 14:52:40 +0900

    클릭시 링크 변경

--- opensea_project/src/App.js
+++ opensea_project/src/App.js
@@ -36,37 +36,8 @@ function App() {
     setAccount(accounts[0]);
   };
 
-  // const addNewErc721Token = async () => {
-  //   const tokenContract = await new web3.eth.Contract(
-  //     erc721Abi,
-  //     newErc721addr,
-  //     { from: account }
-  //   );
-  //   const name = await tokenContract.methods.name().call();
-  //   const symbol = await tokenContract.methods.symbol().call();
-  //   const totalSupply = await tokenContract.methods.totalSupply().call();
-
-  //   let arr = [];
-  //   for (let i = 1; i <= totalSupply; i++) {
-  //     arr.push(i);
-  //   }
-
-  //   for (let tokenId of arr) {
-  //     let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
-  //     if (String(tokenOwner).toLowerCase() === account) {
-  //       let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
-  //       setErc721list((prevState) => {
-  //         return [...prevState, { name, symbol, tokenId, tokenURI }];
-  //       });
-  //     }
-  //   }
-  // };
   return (
     <div className="App">
-      {/* <Nav connectWallet={connectWallet} />
-      <div className="userInfo">Address: {account}</div>
-
-      <TokenList web3={web3} account={account} /> */}
       <Router>
         <Nav connectWallet={connectWallet} />
         <div className="userInfo">Address: {account}</div>
