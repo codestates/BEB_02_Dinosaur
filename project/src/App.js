84b28a9ced142385f115b06fed9e8c4ce7a31ec2
koeunchorong <koasis03@gmail.com>
2022-02-15 17:43:23 +0900

    backup file upload

--- project/src/App.js
+++ project/src/App.js
@@ -7,12 +7,16 @@ import NFTlist from "./components/NFTlist";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import erc721Abi from "./erc721Abi";
 import TokenList from "./components/TokenList";
+import default_Img from "./components/imgs/nft.png";
 
 function App() {
   const [web3, setWeb3] = useState();
   const [account, setAccount] = useState("");
-  const [erc721list, setErc721list] = useState([]); // 자신의 NFT 정보를 저장할 토큰
-  const [newErc721addr, setNewErc721Addr] = useState();
+  // const [erc721list, setErc721list] = useState([]); // 자신의 NFT 정보를 저장할 토큰
+  // const [newErc721addr, setNewErc721Addr] = useState();
+  const onErrorImg = (e) => {
+    e.target.src = default_Img;
+  };
 
   useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
@@ -32,59 +36,19 @@ function App() {
     setAccount(accounts[0]);
   };
 
-  const addNewErc721Token = async () => {
-    const tokenContract = await new web3.eth.Contract(
-      erc721Abi,
-      newErc721addr,
-      { from: account }
-    );
-    const name = await tokenContract.methods.name().call();
-    const symbol = await tokenContract.methods.symbol().call();
-    const totalSupply = await tokenContract.methods.totalSupply().call();
-
-    let arr = [];
-    for (let i = 1; i <= totalSupply; i++) {
-      arr.push(i);
-    }
-
-    for (let tokenId of arr) {
-      let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
-      if (String(tokenOwner).toLowerCase() === account) {
-        let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
-        setErc721list((prevState) => {
-          return [...prevState, { name, symbol, tokenId, tokenURI }];
-        });
-      }
-    }
-  };
   return (
     <div className="App">
-      <Nav connectWallet={connectWallet} />
-      {/* <div className="userInfo">Address: {account}</div> */}
-      {/* <div className="newErc721">
-        <input class ="searchBox_contract"
-          type="text"
-          placeholder="Enter you contract address"
-          onChange={(e) => {
-            setNewErc721Addr(e.target.value);
-          }}
-        ></input>
-        <button class ="w-btn w-btn-gra1 w-btn-gra-anim" onClick={addNewErc721Token}>Find my NFT</button>
-      </div> */}
-      <TokenList web3={web3} account={account} erc721list={erc721list} />
-      {/* router 사용할 경우 NFTlist 부분에 tokenlist 부분 넣기 */}
-      {/* <Router>
-        <Nav connectWallet={connectWallet} />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/nfts" element={<NFTlist />} />
-        </Routes>
-      </Router> */}
       <Router>
+        <Nav connectWallet={connectWallet} />
+        <div className="userInfo">Address: {account}</div>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route
+            path="/nfts"
+            element={<TokenList web3={web3} account={account} />}
+          />
         </Routes>
-      </Router> 
+      </Router>
     </div>
   );
 }
