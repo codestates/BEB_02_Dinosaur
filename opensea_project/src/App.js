2a70837579786803f4c2b995c4096ad3928fb712
koeunchorong <koasis03@gmail.com>
2022-02-15 13:41:21 +0900

    page 나누기

--- opensea_project/src/App.js
+++ opensea_project/src/App.js
@@ -12,9 +12,11 @@ import default_Img from "./components/imgs/nft.png";
 function App() {
   const [web3, setWeb3] = useState();
   const [account, setAccount] = useState("");
-  const [erc721list, setErc721list] = useState([]); // 자신의 NFT 정보를 저장할 토큰
-  const [newErc721addr, setNewErc721Addr] = useState();
-  const onErrorImg = (e) => {e.target.src = default_Img;}
+  // const [erc721list, setErc721list] = useState([]); // 자신의 NFT 정보를 저장할 토큰
+  // const [newErc721addr, setNewErc721Addr] = useState();
+  const onErrorImg = (e) => {
+    e.target.src = default_Img;
+  };
 
   useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
@@ -34,54 +36,48 @@ function App() {
     setAccount(accounts[0]);
   };
 
-  const addNewErc721Token = async () => {
-    const tokenContract = await new web3.eth.Contract(
-      erc721Abi,
-      newErc721addr,
-      { from: account }
-    );
-    const name = await tokenContract.methods.name().call();
-    const symbol = await tokenContract.methods.symbol().call();
-    const totalSupply = await tokenContract.methods.totalSupply().call();
+  // const addNewErc721Token = async () => {
+  //   const tokenContract = await new web3.eth.Contract(
+  //     erc721Abi,
+  //     newErc721addr,
+  //     { from: account }
+  //   );
+  //   const name = await tokenContract.methods.name().call();
+  //   const symbol = await tokenContract.methods.symbol().call();
+  //   const totalSupply = await tokenContract.methods.totalSupply().call();
 
-    let arr = [];
-    for (let i = 1; i <= totalSupply; i++) {
-      arr.push(i);
-    }
+  //   let arr = [];
+  //   for (let i = 1; i <= totalSupply; i++) {
+  //     arr.push(i);
+  //   }
 
-    for (let tokenId of arr) {
-      let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
-      if (String(tokenOwner).toLowerCase() === account) {
-        let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
-        setErc721list((prevState) => {
-          return [...prevState, { name, symbol, tokenId, tokenURI }];
-        });
-      }
-    }
-  };
+  //   for (let tokenId of arr) {
+  //     let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
+  //     if (String(tokenOwner).toLowerCase() === account) {
+  //       let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
+  //       setErc721list((prevState) => {
+  //         return [...prevState, { name, symbol, tokenId, tokenURI }];
+  //       });
+  //     }
+  //   }
+  // };
   return (
     <div className="App">
-      <Nav connectWallet={connectWallet} />
+      {/* <Nav connectWallet={connectWallet} />
       <div className="userInfo">Address: {account}</div>
-      <div className="newErc721">
-        <input class ="searchBox_contract"
-          type="text"
-          placeholder="Enter you contract address"
-          onChange={(e) => {
-            setNewErc721Addr(e.target.value);
-          }}
-        ></input>
-        <button class ="w-btn w-btn-gra1 w-btn-gra-anim" onClick={addNewErc721Token}>Find my NFT</button>
-      </div>
-      <TokenList web3={web3} account={account} erc721list={erc721list} />
-      {/* router 사용할 경우 NFTlist 부분에 tokenlist 부분 넣기 */}
-      {/* <Router>
+
+      <TokenList web3={web3} account={account} /> */}
+      <Router>
         <Nav connectWallet={connectWallet} />
+        <div className="userInfo">Address: {account}</div>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/nfts" element={<NFTlist />} />
+          <Route
+            path="/nfts"
+            element={<TokenList web3={web3} account={account} />}
+          />
         </Routes>
-      </Router> */}
+      </Router>
     </div>
   );
 }
--- opensea_project/src/components/NFTlist.js
+++ opensea_project/src/components/NFTlist.js
@@ -1,11 +1,11 @@
-// import React from "react";
+import React from "react";
 
-// function NFTlist({ account }) {
-//   return (
-//     // <div className="NFTlist">
-//     //   <div className="userInfo">주소: {account}</div>
-//     // </div>
-//   );
-// }
+function NFTlist({ account }) {
+  return (
+    <div className="NFTlist">
+      <div className="userInfo">주소: {account}</div>
+    </div>
+  );
+}
 
-// export default NFTlist;
+export default NFTlist;
