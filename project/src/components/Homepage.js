9fb1e0c3d596bdc614dcdbb2b816b87c5722b585
yonacode <62411918+yonacode@users.noreply.github.com>
2022-02-16 16:30:12 +0900

    mainpage_text and searchbar update

--- project/src/components/Homepage.js
+++ project/src/components/Homepage.js
@@ -6,7 +6,14 @@ function Homepage() {
     <div className="homepage">
       {/* <img src="./imgs/nft.png" className="nftpic" alt="nft" /> */}
       {/* <h2>Discover, collect, and sell extraordinary NFTs</h2> */}
-      <h1 class = "explanation-title">Dinosaur Archive.</h1>
+      <div class = "container">
+        <h1 class = "explanation-title">Dinosaur Collection</h1>
+      </div>
+      {/* <div class = "container">
+        <p class ="notice"> 
+        </p>
+      </div>
+      */}
       <div class = "view">
         <div>
 
@@ -22,7 +29,7 @@ function Homepage() {
                 </a>
                 <div class="nft-item-info">
                   <a href ="https://time.com/collection/slices-of-time/6141243/colin-egan-2015/">
-                    <h2 class="nft-item-title">...</h2>
+                    <h2 class="nft-item-title"></h2>
                     <h1 class="nft-item-title nft-title">Colin Egan <br></br> "Blue Cat" </h1>
                     <small class="nft-item-date">2015</small>
                   </a>
@@ -38,9 +45,9 @@ function Homepage() {
                 </a>
                 <div class="nft-item-info">
                   <a href ="https://time.com/collection/slices-of-time/6141244/vinnie-hager-2115/">
-                    <h2 class="nft-item-title">...</h2>
+                    <h2 class="nft-item-title"></h2>
                     <h1 class="nft-item-title poster-title">Vinnie Hager <br></br> "Send More Mail"</h1>
-                    <small class="nft-item-date">2015</small>
+                    <small class="nft-item-date">2115</small>
                   </a>
                 </div>
               </div>
@@ -54,7 +61,7 @@ function Homepage() {
                 </a>
                 <div class="nft-item-info">
                   <a href ="https://time.com/collection/slices-of-time/6141259/showdeer-1986/">
-                    <h2 class="nft-item-title">...</h2>
+                    <h2 class="nft-item-title"></h2>
                     <h1 class="nft-item-title poster-title">Showdeer <br></br> "Chuck & The Challenger"</h1>
                     <small class="nft-item-date">1986</small>
                   </a>
@@ -70,7 +77,7 @@ function Homepage() {
                 </a>
                 <div class="nft-item-info">
                   <a href ="https://time.com/collection/slices-of-time/6141253/alessandro-gottardo-1991/">
-                    <h2 class="nft-item-title">...</h2>
+                    <h2 class="nft-item-title"></h2>
                     <h1 class="nft-item-title poster-title">Alessandro Gottardo <br></br> "1991"</h1>
                     <small class="nft-item-date">1991</small>
                   </a>
@@ -86,78 +93,85 @@ function Homepage() {
           <div class = "container">
             <div class = "row nft-row">
               <div class = "nft-item col-md-3">
-                <a href="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2015_Colin_Egan_wb.jpg?w=1600&quality=70" class = "nft-item-link">
+                <a href="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2119_Geregamo_wb.jpg?w=1600&quality=70" class = "nft-item-link">
                   <div>
                     <picture>
-                      <img src="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2015_Colin_Egan_wb.jpg?w=1600&quality=70" width = "330px"></img>  
+                      <img src="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2119_Geregamo_wb.jpg?w=1600&quality=70" alt="" title="Gerald Jean Baptiste" width = "330px"></img>  
                     </picture>
                   </div>
                 </a>
                 <div class="nft-item-info">
-                  <a href ="https://time.com/collection/slices-of-time/6141243/colin-egan-2015/">
-                    <h2 class="nft-item-title">...</h2>
-                    <h1 class="nft-item-title nft-title">Colin Egan <br></br> "Blue Cat" </h1>
-                    <small class="nft-item-date">2015</small>
+                  <a href ="https://time.com/collection/slices-of-time/6141242/geregamo-2119/">
+                    <h2 class="nft-item-title"></h2>
+                    <h1 class="nft-item-title nft-title">Gerald Jean Baptiste <br></br> "Reach For Redemption" </h1>
+                    <small class="nft-item-date">2119</small>
                   </a>
                 </div>
               </div>
               <div class = "nft-item col-md-3">
-                <a href="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2115_Vinnie_Hager_wb.jpg?w=1600&quality=70" class = "nft-item-link">
+                <a href="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_1969_Brian_Stauffer_wb.jpg?w=1600&quality=70" class = "nft-item-link">
                   <div>
                     <picture>
-                      <img src="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2115_Vinnie_Hager_wb.jpg?w=1600&quality=70" alt="" title="Vinnie Hager, 2115" width = "330px"></img>  
+                      <img src="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_1969_Brian_Stauffer_wb.jpg?w=1600&quality=70" alt="" title="Vinnie Hager, 2115" width = "330px"></img>  
                     </picture>
                   </div>
                 </a>
                 <div class="nft-item-info">
-                  <a href ="https://time.com/collection/slices-of-time/6141244/vinnie-hager-2115/">
-                    <h2 class="nft-item-title">...</h2>
-                    <h1 class="nft-item-title poster-title">Vinnie Hager <br></br> "Send More Mail"</h1>
-                    <small class="nft-item-date">2015</small>
+                  <a href ="https://time.com/collection/slices-of-time/6141242/geregamo-2119/">
+                    <h2 class="nft-item-title"></h2>
+                    <h1 class="nft-item-title poster-title">Brian Stauffer <br></br> "The Year That Took <br></br>Us To The Moon"</h1>
+                    <small class="nft-item-date">1969</small>
                   </a>
                 </div>
               </div>
               <div class = "nft-item col-md-3">
-                <a href="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_1986_CHRIS_BIGALKE_wb.gif?w=1600&quality=70" class = "nft-item-link">
+                <a href="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2069_Harry_Campbell_wb.jpg?w=1600&quality=70" class = "nft-item-link">
                   <div>
                     <picture>
-                      <img src="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_1986_CHRIS_BIGALKE_wb.gif?w=1600&quality=70" alt="" title="Showdeer, 1986" width = "330px"></img>  
+                      <img src="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2069_Harry_Campbell_wb.jpg?w=1600&quality=70" alt="" title="Harry Campbell, 2069" width = "330px"></img>  
                     </picture>
                   </div>
                 </a>
                 <div class="nft-item-info">
-                  <a href ="https://time.com/collection/slices-of-time/6141259/showdeer-1986/">
-                    <h2 class="nft-item-title">...</h2>
-                    <h1 class="nft-item-title poster-title">Showdeer <br></br> "Chuck & The Challenger"</h1>
-                    <small class="nft-item-date">1986</small>
+                  <a href ="https://time.com/collection/slices-of-time/6141270/harry-campbell-2069/">
+                    <h2 class="nft-item-title"></h2>
+                    <h1 class="nft-item-title poster-title">Harry Campbell<br></br>Porthole</h1>
+                    <small class="nft-item-date">2069</small>
                   </a>
                 </div>
               </div>
               <div class = "nft-item col-md-3">
-                <a href="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_1991_Alessandro_Gottardo_wb.jpg?w=1600&quality=70" class = "nft-item-link">
+                <a href="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2020_Drift_wb.jpg?w=1600&quality=70" class = "nft-item-link">
                   <div>
                     <picture>
-                      <img src="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_1991_Alessandro_Gottardo_wb.jpg?w=1600&quality=70" alt="" title="Alessandro Gottardo, 1991" width= "330px"></img>  
+                      <img src="https://api.time.com/wp-content/uploads/2022/01/TIMEPieces_Slice_of_TIME_2020_Drift_wb.jpg?w=1600&quality=70" alt="" title="Isaac Drift Wright, 2020" width= "330px"></img>  
                     </picture>
                   </div>
                 </a>
                 <div class="nft-item-info">
-                  <a href ="https://time.com/collection/slices-of-time/6141253/alessandro-gottardo-1991/">
-                    <h2 class="nft-item-title">...</h2>
-                    <h1 class="nft-item-title poster-title">Alessandro Gottardo <br></br> "1991"</h1>
-                    <small class="nft-item-date">1991</small>
+                  <a href ="https://time.com/collection/slices-of-time/6141239/drift-2020/">
+                    <h2 class="nft-item-title"></h2>
+                    <h1 class="nft-item-title poster-title">Isaac Wright<br></br>As the World Turns</h1>
+                    <small class="nft-item-date">2020</small>
                   </a>
                 </div>
               </div>
             </div>
           </div>
 
+          <div>
+            <span></span>
+          </div>
+
+
+
+
         </div>
       </div>
       <footer>
         <div class="footer">
 					<div class="footer_text">
-							© 2022 Dinosaur. All Rights Reserved.
+          All of the licensed content is owned by Time. (used only for the test project in class)<br></br>
 					</div>
 				</div>
 			</footer>
